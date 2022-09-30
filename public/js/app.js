9590483bd240912f5eacf86bc55c7f1184489564
Olivoz <lucas.sivars@gmail.com>
2022-09-30 11:51:10 +0200

    Load cars from json

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,25 @@ function appendElement(target, elementName, content, id) {
   target.appendChild(element);
 }
 
+function loadCars() {
+  let data = JSON.parse(this.responseText);
+  data.cars.forEach((car) => {
+    appendElement(carListElement, "li", car);
+  });
+}
+
 function loadStructure() {
   let data = JSON.parse(this.responseText);
   data.elements.forEach((elementJSON) => {
     appendElement(app, elementJSON.tag, elementJSON.content, elementJSON.id);
   });
+
+  loadCMSButtonElement.onclick = () => {
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", loadCars);
+    req.open("GET", "http://localhost:5500/public/json/cars.json");
+    req.send();
+  };
 }
 
 function build() {
